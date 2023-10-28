b430eaf49feec5e79b16891d04f9c34f56665c87
Ishank Gupta <ishank1995@gmail.com>
2023-10-28 12:52:09 +0530

    optimised code

--- password_generator.py
+++ password_generator.py
@@ -7,59 +7,35 @@ numbers = string.digits
 special_characters = string.punctuation
 
 def generate_password(length, mix_case_flag, numbers_flag, special_characters_flag):
-    password_string = ''
+    password_string = lowercase_alphabets
     if mix_case_flag:
-        password_string += lowercase_alphabets + uppercase_alphabets
-    else:
-        password_string += lowercase_alphabets
-        
+        password_string += uppercase_alphabets
     if numbers_flag:
         password_string += numbers
     if special_characters_flag:
         password_string += special_characters
     
-    matches_criteria = False
-    while not matches_criteria:
-        temp_password = ''
-        for i in range(length):
-            temp_password += random.choice(password_string)
-        
-        # Check if the password contains characters from the required sets
-        if (mix_case_flag and any(char.islower() for char in temp_password) and any(char.isupper() for char in temp_password)) or not mix_case_flag:
-            if numbers_flag and any(char.isdigit() for char in temp_password) or not numbers_flag:
-                if special_characters_flag and any(char in special_characters for char in temp_password) or not special_characters_flag:
-                    matches_criteria = True
-                    
-    print(f'length: {length}, mix_case_flag: {mix_case_flag}, numbers_flag: {numbers_flag}, special_characters_flag: {special_characters_flag}')
-    
-    print(f'your generated password is: {temp_password}')
+    temp_password = ''.join(random.choice(password_string) for _ in range(length))
+    return temp_password
 
-length = input('enter password length: ')
+def get_password_length():
+    while True:
+        length = input('Enter password length: ')
+        if length.isdigit():
+            return int(length)
+        print('Please enter a valid number.')
 
-while not length.isdigit():
-    print(' please enter a valid number')
-    length = input('enter password length: ')
+def get_user_choice(message):
+    return input(message).lower() == 'y'
 
-length_int = int(length)
+def main():
+    length = get_password_length()
+    mix_case_flag = get_user_choice('Mix case? (y for yes): ')
+    numbers_flag = get_user_choice('Do you want numbers in your password? (y for yes): ')
+    special_characters_flag = get_user_choice('Do you want special characters in your password? (y for yes): ')
     
-mix_case_input = input('mix case? (y for yes)')
+    password = generate_password(length, mix_case_flag, numbers_flag, special_characters_flag)
+    print(f'Your generated password is: {password}')
 
-if mix_case_input == 'y':
-    mix_case_flag = True
-else: 
-    mix_case_flag = False
-    
-numbers_input = input('Do you want numbers in your password? (y for yes)')
-if numbers_input == 'y':
-    numbers_flag = True
-else: 
-    numbers_flag = False
-    
-    
-special_characters_input = input('Do you want special characters in your password? (y for yes)')
-if special_characters_input == 'y':
-    special_characters_flag = True
-else: 
-    special_characters_flag = False
-
-generate_password(length_int, mix_case_flag, numbers_flag, special_characters_flag)
+if __name__ == "__main__":
+    main()
